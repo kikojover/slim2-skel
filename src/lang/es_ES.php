90d248392111e3d4f25cc5357c5920fa3794827e
Kiko Jover <kiko@kikojover.es>
2016-07-25 18:37:02 +0200

    Translate some templates

--- src/lang/es_ES.php
+++ src/lang/es_ES.php
@@ -1,7 +1,32 @@
 <?php
 return array(
+    //Notifications
+    //ERROR
+    'Login field is required.' => 'Es necesario introducir el usuario.',
+    'Password field is required.' => 'Es necesario introducir la contraseña.',
+    'User was not found.' => 'El usuario no existe',
+    'Wrong password, try again.' => 'Contraseña incorrecta, vuelve a probar.',
+    
+    //INFO
+    "You've logged out." => 'Has salido de la aplicación.',
+    "We've sent an email with instrucions" => 'Hemos enviado un email con las instrucciones.',
+    
+    //Login
+    'Login Form' => 'Acceso',
+    'Log in' => 'Acceso',
+    'Submit' => 'Enviar',
+    'Username' => 'Usuario',
+    'Password' => 'Contraseña',
+    'Lost your password?' => 'Recuperar contraseña',
+    'Lost password' => 'Recuperar contraseña',
+    
+    //General
+    'Profile' => 'Perfil',
+    'Log Out' => 'Salir',
+    'Home' => 'Inicio',
     'Action' => 'Acción',
     'Create' => 'Crear',
     'Edit' => 'Editar',
     'Delete' => 'Borrar',
+    'Users' => 'Usuarios'
 );
