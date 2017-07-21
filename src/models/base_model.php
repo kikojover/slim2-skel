68081a99d409ada184791c0d288d9d7a2a1aced7
Kiko Jover <kiko@kikojover.es>
2017-07-21 18:19:54 +0200

    Add properties to base model

--- src/models/base_model.php
+++ src/models/base_model.php
@@ -7,12 +7,17 @@ class Base_model extends \Illuminate\Database\Eloquent\Model
   public $list_fields = array();
   public static $user = null;
   public $template = null;
+  public $template_view = null;
+  public $params = null;
   
   public function __construct(){
       global $app;
       
       parent::__construct();
       self::$user = $app->sentry->getUser();
+  }
+
+  public function format(){
   }
 
 }
