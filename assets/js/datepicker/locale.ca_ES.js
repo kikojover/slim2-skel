0e5b58c5e23c15ada1a4ee7013ea1ebc642a8004
Kiko Jover <kiko@kikojover.es>
2017-09-14 07:26:40 +0200

    Update datepicker and localisation

---
+++ assets/js/datepicker/locale.ca_ES.js
@@ -0,0 +1,33 @@
+var drp_locale_ca_ES = {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "applyLabel": "Aplicar",
+        "cancelLabel": "Cancelar",
+        "fromLabel": "Des de",
+        "toLabel": "Fins a",
+        "customRangeLabel": "Personalitzat",
+        "daysOfWeek": [
+            "Diumenge",
+            "Dilluns",
+            "Dimarts",
+            "Dimecres",
+            "Dijous",
+            "Divendres",
+            "Dissabte"
+        ],
+        "monthNames": [
+            "Gener",
+            "Febrer",
+            "Març",
+            "Abril",
+            "Maig",
+            "Juny",
+            "Juliol",
+            "Agust",
+            "Setembre",
+            "Octubre",
+            "Novembre",
+            "Desembre"
+        ],
+        "firstDay": 1
+    };
---
+++ assets/js/datepicker/locale.es_ES.js
@@ -0,0 +1,33 @@
+var drp_locale_es_ES = {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "applyLabel": "Aplicar",
+        "cancelLabel": "Cancelar",
+        "fromLabel": "Desde",
+        "toLabel": "Hasta",
+        "customRangeLabel": "Personalizado",
+        "daysOfWeek": [
+            "Do",
+            "Lu",
+            "Ma",
+            "Mi",
+            "Ju",
+            "Vi",
+            "Sa"
+        ],
+        "monthNames": [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agusto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        ],
+        "firstDay": 1
+    };
